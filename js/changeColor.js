d5dd2461c22f4d2067d21737e37cce0e83544f7b
mvn2004 <mvn2004@mail.ru>
2018-05-04 15:40:37 +0300

    Редактирование HTML & JS

--- js/changeColor.js
+++ js/changeColor.js
@@ -1,15 +1,20 @@
-function loaded() {
-    changeColor();
-    var today = new Date();
-    var date = new Date('time');
-}
-
+//"use strict";
+var today = new Date();
+var datesStr = document.getElementsByClassName("cellDate");
+//Изменение цвета шрифта от даты
 function changeColor() {
-    if (today < date) {
-        document.body.table2.rowD.cell color.style.color: #258D57;
-    } else if (today > date) {
-        document.body.table2.rowD.cell color.style.color: #C53929;
-    } else {
-        document.body.table2.rowD.cell color.style.color: #F09300;
+    var dateStrLen = datesStr.length;    
+    for (var i = 0; i < dateStrLen; i++) {
+//    for (let dateStr of datesStr) { // только для Chrome & Firefox
+        var date = new Date(datesStr[i].innerText.replace(/(\d+).(\d+).(\d+)/, '$3/$2/$1'));
+        if (today < date) {
+            datesStr[i].style.color = "#258D57";
+        } else if (today > date) {
+            datesStr[i].style.color = "#C53929";
+        } else {
+            datesStr[i].style.color = "#F09300";
+        }
     }
 }
+
+changeColor();
