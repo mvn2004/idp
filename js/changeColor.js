9f83e7215e77b5913b926251a0f39f21f0fb1241
mvn2004 <mvn2004@mail.ru>
2018-07-31 17:59:53 +0300

    Редактирование скриптов

--- js/changeColor.js
+++ js/changeColor.js
@@ -1,12 +1,15 @@
 //"use strict";
 var today = new Date();
 var datesStr = document.getElementsByClassName("cellDate");
+
 //Изменение цвета шрифта от даты
 function changeColor() {
-    var dateStrLen = datesStr.length;    
+    var dateStrLen = datesStr.length;
+    
     for (var i = 0; i < dateStrLen; i++) {
 //    for (let dateStr of datesStr) { // только для Chrome & Firefox
         var date = new Date(datesStr[i].innerText.replace(/(\d+).(\d+).(\d+)/, '$3/$2/$1'));
+        
         if (today < date) {
             datesStr[i].style.color = "#258D57";
         } else if (today > date) {
